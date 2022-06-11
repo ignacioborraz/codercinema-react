d07d8fce7633e7cf03a3575baeb43c2a198cb15e
ignacioborraz <ignacioborraz@hotmail.com>
2022-06-10 21:38:32 -0300

    pkm

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,30 +1,26 @@
-let urlMove = "https://pokeapi.co/api/v2/move/" //url de la api que contiene los datos de UN MOVIMIENTO
-//lamentablemente, no existe un url para obtener TODOS los movimientos
+let urlPkm = "https://pokeapi.co/api/v2/pokemon/" //url de la api que contiene los datos de UN POKEMON
+//lamentablemente, no existe un url para obtener TODOS los pokemon
 //como no existe ese llamado, vamos a tener que iterar este llamado, la cantidad de veces que querramos
 
-let moves = [] //array que va a contener las propiedades de todos los movimientos fetcheados
+let pkm = [] //array que va a contener las propiedades de todos los pkm fetcheados
 
-async function fetchMove(id) { //funcion que trae los datos de UN SOLO movimiento
-    let response = await fetch(urlMove+id) //lo primero que tiene que hacer la funcion es ESPERAR la carga de un archivo (base de datos)
+async function fetchPkm(id) { //funcion que trae los datos de UN SOLO pkm
+    let response = await fetch(urlPkm+id) //lo primero que tiene que hacer la funcion es ESPERAR la carga de un archivo (base de datos)
     let data = await response.json() // espero la transformacion del json
     //console.log(data)
     return data
 }
 
-async function fetchMoves() { //funcion que trae los datos de TODOS los movimientos (50)
-    for (let i=1; i<=20; i++) { //iteracion de la funcion que me trae los datos de UN SOLO movimiento
-        let data = await fetchMove(i) //consumo la api
-        moves.push(data) //pusheo los datos en un array
+async function fetchPokems() { //funcion que trae los datos de TODOS los pkm (25)
+    for (let i=1; i<=25; i++) { //iteracion de la funcion que me trae los datos de UN SOLO pkm
+        let data = await fetchPkm(i) //consumo la api
+        pkm.push(data) //pusheo los datos en un array
     }
-    //console.log(moves)
-    let sorted = sortedMoves(moves) //array con los movimientos ordenados por PRECISION
-    let filtered = filteredMoves(sorted) //array con los movimientos FILTRADOS por tipo de ataque y ordenados por precision
-    createMoveMinMax(sorted)
-    createMoves(sorted)
-    createProm(filtered)
+    console.log(pkm)
+    createPkm(pkm)
 }
 
-fetchMoves()
+fetchPokems()
 
 function createMove(movesArraysortedByCat,movesArraySortedByAssi) { //funcion que imprime TODOS los movimientos con su nombre/presición/tipo
     let move = "" //creo la variable que va a almacenar TODOS los templates
@@ -66,19 +62,17 @@ function createMoveMinMax(movesArray) { //funcion que imprime TODOS los movimien
     document.querySelector("#accuracyTable1").innerHTML += move //imprimo en el selector correspondiente
 }
 
-function createMoves(movesArray) { //funcion que imprime TODOS los movimientos con su nombre/presición/tipo
-    let move = "" //creo la variable que va a almacenar TODOS los templates
-    movesArray.forEach((everyMove,index) => {//recorro el array que entra como parámetro para guardar en cada ciclo el template correspondiente
-            move += `
-            <tr class="table-light d-flex justify-content-center">
-                <td scope="row" class="d-flex justify-content-center align-items-center text-center col-1">${index+1}</th>
-                <td scope="row" class="d-flex justify-content-center align-items-center text-center col-1">${everyMove.name}</th>
-                <td scope="row" class="d-flex justify-content-center align-items-center text-center col-1">${everyMove.accuracy}</th>
-                <td scope="row" class="d-flex justify-content-center align-items-center text-center col-1">${everyMove.type.name}</th>
-            </tr>
+function createPkm(arrayPkm) { //funcion que imprime TODOS los pkm
+    let pkm = "" //creo la variable que va a almacenar TODOS los templates
+    arrayPkm.forEach((everyPkm) => {//recorro el array que entra como parámetro para guardar en cada ciclo el template correspondiente
+            pkm += `
+            <div>
+                <p class="d-flex justify-content-center align-items-center text-center">${everyPkm.name}</p>
+                <img class="d-flex justify-content-center align-items-center text-center" src=${everyPkm.sprites.front_default}>
+            </div>
             `
     })
-    document.querySelector("#accuracyTable2").innerHTML += move //imprimo en el selector correspondiente
+    document.querySelector("#pkmTable").innerHTML += pkm //imprimo en el selector correspondiente
 }
 
 function createProm(movesArray) { //funcion que imprime la tabla promedio de la precision por tipo de ataque
