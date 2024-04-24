87c74c5097f6f8596cff315e63c207348cec0494
igna <ignacioborraz@hotmail.com>
2024-04-24 12:22:52 -0300

    interfaces

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,13 +1,8 @@
-import Product from "../interfaces/product";
+import Product from "../interfaces/ProductCard";
 import { Link } from "react-router-dom";
 
-interface Props {
-  product: Product;
-}
-
-export default function ProductCard(props: Props) {
-  const { product } = props;
-  const { id, title, price, colors, images } = product;
+export default function ProductCard(props: Product) {
+  const { id, title, price, color, image } = props;
   return (
     <Link
       className="bg-[#f0f0f0] text-[#383838] rounded-md w-[250px] m-[15px] no-underline"
@@ -15,14 +10,14 @@ export default function ProductCard(props: Props) {
     >
       <img
         className="w-full h-[250px] object-cover object-center rounded-t-md"
-        src={images[0]}
+        src={image}
         alt={title}
       />
       <div className="p-[20px]">
         <span className="text-[20px] font-bold leading-[25px] block">
           {title}
         </span>
-        <span className="text-[12px] text-[#383838] mb-[5px]">{colors[0]}</span>
+        <span className="text-[12px] text-[#383838] mb-[5px]">{color}</span>
         <div className="flex my-[5px] justify-between items-center">
           <span className="text-[20px] font-bold text-[#428f13]">
             $ {price}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 import NavBar from "../components/NavBar";
 import ProductCard from "../components/ProductCard";
 import products from "../assets/products";
-import Product from "../interfaces/product";
+import Product from "../interfaces/Prod";
 
 function Home() {
   return (
@@ -16,7 +16,14 @@ function Home() {
           id="products"
         >
           {products.map((each: Product) => (
-            <ProductCard key={each.id} product={each} />
+            <ProductCard
+              key={each.id}
+              id={each.id}
+              title={each.title}
+              price={each.price}
+              color={each.colors[0]}
+              image={each.images[0]}
+            />
           ))}
         </div>
       </main>
