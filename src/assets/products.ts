a533a9293018321902e9ba5c070f85c95e3603ef
igna <ignacioborraz@hotmail.com>
2024-04-25 17:35:05 -0300

    axios

--- src/assets/products.ts
+++ src/assets/products.ts
@@ -1,4 +1,4 @@
-import Product from "../interfaces/product";
+import Product from "../interfaces/Product";
 
 const products: Array<Product> = [
   {
--- src/views/Details.tsx
+++ src/views/Details.tsx
@@ -1,5 +1,5 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import products from "../assets/products";
 import Footer from "../components/Footer";
 import NavBar from "../components/NavBar";
 import Hero from "../components/Hero";
@@ -8,11 +8,33 @@ import Thumbs from "../components/Thumbs";
 import Description from "../components/Description";
 import Checkout from "../components/Checkout";
 import Product from "../interfaces/Product";
+import axios from "axios";
 
 function Details() {
   const { id } = useParams();
-  const product: Product = products.find((each) => each.id === id);
-  const onsale: Product[] = products.filter((each) => each.onsale);
+  const [product, setProduct] = useState<Product>({
+    id: "",
+    title: "",
+    price: 0,
+    images: [],
+    colors: [],
+  });
+  const [onsale, setOnSale] = useState<Product[]>([]);
+  useEffect(() => {
+    axios("/products.json")
+      .then((res) => {
+        const products: Array<Product> = res.data;
+        const detailProduct: Product | undefined = products.find(
+          (each) => each.id === id
+        );
+        detailProduct && setProduct(detailProduct);
+        const filterProducts: Array<Product> = products.filter(
+          (each) => each.onsale
+        );
+        filterProducts.length > 0 && setOnSale(filterProducts);
+      })
+      .catch((err) => console.log(err));
+  }, [id]);
   return (
     <>
       <NavBar />
